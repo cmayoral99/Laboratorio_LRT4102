259542321dd11442502dff9acceec129e882e65b
Claudia Mayoral <141690369+cmayoral99@users.noreply.github.com>
2025-02-23 21:38:26 -0600

    Update problema_1.py

--- Lab1/src/problema_1.py
+++ Lab1/src/problema_1.py
@@ -1,1 +1,10 @@
+# Le pido al usuario que ingrese un número entero positivo y lo convierto a entero
+n = int(input("Introduce un número entero positivo: "))
+
+# Calculo la suma de los primeros n números usando la fórmula: (n * (n + 1)) / 2
+# Uso // para obtener un resultado entero 
+suma = (n * (n + 1)) // 2
+
+# Imprimir
+print(f"La suma de los primeros {n} enteros positivos es: {suma}")
 
